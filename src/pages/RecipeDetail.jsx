3e784edd6c7690c226e64d1610d6bb4fcb8d111c
Monica Davila <monica.davila510@gmail.com>
2026-05-04 18:53:30 -0500

    Fetch and display recipe details

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -1,12 +1,28 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 function RecipeDetail() {
   const { recipeId } = useParams();
+  const [recipe, setRecipe] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setRecipe(data.meals[0]);
+      });
+  }, [recipeId]);
+
+  if (!recipe) {
+    return <p>Loading recipe details...</p>;
+  }
 
   return (
     <div>
-      <h2>Recipe Detail</h2>
-      <p>Recipe ID: {recipeId}</p>
+      <Link to="/">Back to recipes</Link>
+      <h2>{recipe.strMeal}</h2>
+      <img src={recipe.strMealThumb} alt={recipe.strMeal} width="300" />
+      <p>{recipe.strInstructions}</p>
     </div>
   );
 }
