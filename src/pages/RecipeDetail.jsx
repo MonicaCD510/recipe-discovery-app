c8919559bfcb5c329a3bd88a1799f8cf61034a44
Monica Davila <monica.davila510@gmail.com>
2026-05-04 18:49:59 -0500

    Create recipe detail page

---
+++ src/pages/RecipeDetail.jsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+
+function RecipeDetail() {
+  const { recipeId } = useParams();
+
+  return (
+    <div>
+      <h2>Recipe Detail</h2>
+      <p>Recipe ID: {recipeId}</p>
+    </div>
+  );
+}
+
+export default RecipeDetail;
