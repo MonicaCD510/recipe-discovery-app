870daec4301157c5a255dbb05e649f73d00db7da
Monica Davila <monica.davila510@gmail.com>
2026-05-04 18:46:10 -0500

    Add detail links to recipe cards

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 function RecipeCard({ recipe }) {
   return (
     <div className="recipe-card">
       <img src={recipe.image} alt={recipe.name} width="100%" />
       <p>{recipe.name}</p>
+      <Link to={`/recipe/${recipe.id}`}>View Details</Link>
     </div>
   );
 }
